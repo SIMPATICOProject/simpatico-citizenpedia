c6eb32e679dccd3e22d5547c1e4e073cc44bdf49
gdimodica <gdimodica@gmail.com>
2018-03-06 14:50:58 +0100

    remove repos

--- cpd/db-restore.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-PILOT=$1
-if ! [[ "$PILOT" =~ ^(trento|galicia|sheffield|all)$ ]]
-then
-  echo "please provide a pilot ID (trento|galicia|sheffield)"
-  exit 1
-fi
-mongorestore --dir "dump/$PILOT" --nsInclude 'cpd.*' --drop --gzip
--- cpd/src/main/deploy-bundle/stop.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$0")"
-if [ -f cpd.pid ]; then
-  echo "cpd-server is going to be killed..."
-  kill $(cat cpd.pid)
-  rm cpd.pid
-else
-  echo "pid file not found, is the server running?"
-fi
